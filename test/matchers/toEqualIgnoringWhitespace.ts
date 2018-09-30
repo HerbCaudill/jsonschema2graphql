cadd03067e06ff00ca0fb2ea0db7839f38c5c582
Herb Caudill <herb@caudillweb.com>
2018-09-30 15:50:07 +0200

    toEqualIgnoringWhitespace: preserve linefeeds to make diff easier

--- test/matchers/toEqualIgnoringWhitespace.ts
+++ test/matchers/toEqualIgnoringWhitespace.ts
@@ -1,29 +1,26 @@
-import {
-  matcherHint,
-  printReceived,
-  printExpected,
-} from 'jest-matcher-utils'
+import { matcherHint, printReceived, printExpected } from 'jest-matcher-utils'
 import diff from 'jest-diff'
 
 const compressWhitespace = (a: string[]) =>
-  a.map(s => s.replace(/\s+/g, ` `).trim())
+  a.map(s =>
+    s
+      .replace(/^\s*$(?:\r\n?|\n)/gm, ``) // remove empty lines
+      .replace(/[ \t]+/g, ` `) // collapse double spaces
+      .replace(/(\s*(?:\r\n?|\n)\s*)+/g, `\r\n`) // remove space before/after linefeeds
+      .trim()
+  )
 
 const name = `toEqualIgnoringWhitespace`
 
 const toEqualIgnoringWhitespace = (actual: string, expected: string) => {
   const [actualComp, expectedComp] = compressWhitespace([actual, expected])
   const pass = actualComp === expectedComp
-  const actualVsReceivedMsg =
-    `Expected value (compressed whitespace) to equal:\n` +
-    `  ${printExpected(expectedComp)}\n` +
-    `Received value (compressed whitespace):\n` +
-    `  ${printReceived(actualComp)}`
   const message = pass
-    ? () => `${matcherHint(`.not.${name}`)}\n\n${actualVsReceivedMsg}\n\n`
+    ? () => `${matcherHint(`.not.${name}`)}\n\n`
     : () => {
         const diffString = diff(expectedComp, actualComp)
         return (
-          `${matcherHint(`.${name}`)}\n\n${actualVsReceivedMsg}\n\n` +
+          `${matcherHint(`.${name}`)}\n\n` +
           `${diffString ? `\n\nDifference:\n\n${diffString}` : ``}`
         )
       }
