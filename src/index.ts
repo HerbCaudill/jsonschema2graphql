70aec296acca12a31e3612b5d9dede7b8dbed586
Herb Caudill <herb@caudillweb.com>
2018-10-02 15:57:31 +0200

    npm publish sorted out 🤞

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export * from './convert'
+import convert from './convert'
+
+export default convert
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,19 +1,23 @@
 import { GraphQLObjectType, GraphQLType } from 'graphql'
 import { JSONSchema7 } from 'json-schema'
 
-export interface GraphQLTypeMap {
-  [name: string]: GraphQLType
+declare namespace jsonschema2graphql {
+  export interface GraphQLTypeMap {
+    [name: string]: GraphQLType
+  }
+
+  export type EntryPointBuilder = (
+    types: GraphQLTypeMap
+  ) => {
+    query: GraphQLObjectType
+    mutation?: GraphQLObjectType
+    subscription?: GraphQLObjectType
+  }
+
+  export interface ConvertParams {
+    jsonSchema: JSONSchema7 | JSONSchema7[] | string | string[]
+    entryPoints?: EntryPointBuilder
+  }
 }
 
-export type EntryPointBuilder = (
-  types: GraphQLTypeMap
-) => {
-  query: GraphQLObjectType
-  mutation?: GraphQLObjectType
-  subscription?: GraphQLObjectType
-}
-
-export interface ConvertParams {
-  jsonSchema: JSONSchema7 | JSONSchema7[] | string | string[]
-  entryPoints?: EntryPointBuilder
-}
+export = jsonschema2graphql
